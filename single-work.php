6cf51c82088f90fd3898df03bc65a83385c3f62f
Jamaludin Rajalu <jrajalu@gmail.com>
2015-04-04 01:00:39 +0800

    no message

--- single-work.php
+++ single-work.php
@@ -8,7 +8,7 @@ get_header(); ?>
   <div class="col span_12_of_12 article">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <h1><?php the_title(); ?></h1>
-  <img src="<?php echo get_post_meta( get_the_ID(), '_jrwtdw_work_image', true ); ?>" alt="<?php the_title(); ?>">
+   <?php the_post_thumbnail( 'full', array('class' => 'project-image') ); ?>
   <p><?php the_content(); ?></p>
   <?php endwhile; else: ?>
     <p>Page not found.</p>
